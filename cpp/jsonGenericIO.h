7ef402b54a9a0eb297aeff7139c72eb54abffaa2
Bob Clapp <bob@sep.stanford.edu>
2017-03-17 20:03:23 -0700

    adedded additional function to baseclass

--- cpp/jsonGenericIO.h
+++ cpp/jsonGenericIO.h
@@ -4,23 +4,30 @@
 #include "jsonGenericFile.h"
 #include "jsonParamObj.h"
 #include <fstream>      // std::ifstream
-namespace SEP{
-class jsonGenericIO: public genericIO{
-  public:
-   jsonGenericIO(){;}
-   jsonGenericIO( const int argc,   char **argv){ initJsonPars(argc,argv);}
-   void initJsonPars( const int argc,  char **argv);
-   std::shared_ptr<Json::Value> getArgs(){ return jsonArgs;}
-    virtual std::shared_ptr<genericRegFile> getRegFileTag(const std::string &tag, const std::string& name,const usage_code usage);
-    virtual std::shared_ptr<genericIrregFile> getIrregFileTag(const std::string &tag,const std::string &name, const usage_code usage);
-    virtual void close();
-    virtual std::shared_ptr<paramObj>  getParamObj();
+namespace SEP {
+class jsonGenericIO : public genericIO {
+public:
+jsonGenericIO(){
+	;
+}
+jsonGenericIO( const int argc,   char **argv){
+	initJsonPars(argc,argv);
+}
+void initJsonPars( const int argc,  char **argv);
+std::shared_ptr<Json::Value> getArgs(){
+	return jsonArgs;
+}
+virtual std::shared_ptr<genericRegFile> getRegFileTag(const std::string &tag, const std::string& name,const usage_code usage);
+virtual std::shared_ptr<genericIrregFile> getIrregFileTag(const std::string &tag,const std::string &name, const usage_code usage);
+virtual void close();
+virtual std::shared_ptr<paramObj>  getParamObj();
 
-    private:
-      std::ifstream inps;
-      std::shared_ptr<Json::Value> jsonArgs;
-      bool _init;
-      
+
+private:
+std::ifstream inps;
+std::shared_ptr<Json::Value> jsonArgs;
+bool _init;
+
 
 
 };

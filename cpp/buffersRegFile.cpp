b53f43c31a01324f4f09f192cf002130b560b7cb
Bob Clapp <bob@sep.stanford.edu>
2019-04-11 04:57:40 +0000

    fix

--- cpp/buffersRegFile.cpp
+++ cpp/buffersRegFile.cpp
@@ -6,14 +6,19 @@
 using namespace SEP;
 
 void buffersRegFile::writeDescription() {
+	std::cerr<<"iwjhere  descripotion"<<std::endl;
   std::shared_ptr<hypercube> hyper = getHyper();
+	std::cerr<<"if1write descripotion"<<std::endl;
   std::vector<axis> axes = hyper->returnAxes(hyper->getNdim());
+	std::cerr<<"ia1write descripotion"<<std::endl;
   for (int i = 1; i <= axes.size(); i++) {
     putInt(std::string("n") + std::to_string(i), axes[i - 1].n);
     putFloat(std::string("o") + std::to_string(i), axes[i - 1].o);
     putFloat(std::string("d") + std::to_string(i), axes[i - 1].d);
     putString(std::string("label") + std::to_string(i), axes[i - 1].label);
   }
+	std::cerr<<"in2write descripotion"<<std::endl;
   jsonArgs["bufferInfo"] = _bufs->getDescription();
   jsonArgs["dataType"] = getTypeString(getDataType());
+	std::cerr<<"in3write descripotion"<<std::endl;
 }
--- unit-tests/GCPIO.cc
+++ unit-tests/GCPIO.cc
@@ -3,29 +3,30 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include "float4Dreg.high_resolution_clock"
+#include "float4DReg.h"
 #include "google/cloud/status_or.h"
 #include "google/cloud/storage/client.h"
 #include "google/cloud/storage/oauth2/google_credentials.h"
 #include "ioModes.h"
 #include "ioTypes.h"
+#include "blocking.h"
 using namespace std::chrono;
 
 using std::string;
-using namespace SEP::IO;
+using namespace SEP;
 
 std::shared_ptr<float4DReg> array(std::shared_ptr<SEP::hypercube> hyper) {
   std::shared_ptr<float4DReg> buf(new float4DReg(hyper));
   float *v = buf->getVals();
   for (auto ii = 0; ii < buf->getHyper()->getN123(); ii++)
-    ar[ii] = (float)rand() / ((float)RAND_MAX) + i1 * .4 + 5.;
-}
-return array;
+    v[ii] = (float)rand() / ((float)RAND_MAX) + ii * .04 + 5.;
+return buf;
 }
 
 TEST(TESTBucketCreation, gcpBuffers) {
   std::vector<SEP::axis> axes;
-  long long n = 200;
+  long long n = 40;
+  //long long n = 200;
   long long n123 = 1;
   int ndim = 4;
   std::vector<int> ns(ndim, n), fs(ndim, 0), js(ndim, 1);
@@ -34,9 +35,12 @@ TEST(TESTBucketCreation, gcpBuffers) {
     axes.push_back(SEP::axis(n));
   }
 
+
+  std::cerr<<"where do i die 1 "<<std::endl;
   std::shared_ptr<SEP::hypercube> hyper(new SEP::hypercube(axes));
   std::shared_ptr<float4DReg> ar = array(hyper);
 
+  std::cerr<<"where1do i die 1 "<<std::endl;
   std::string bucket = std::string("testbucket994");
   std::string bucket1 = bucket + std::string("/dataset1");
   std::string bucket2 = bucket + std::string("/dataset2");
@@ -44,42 +48,52 @@ TEST(TESTBucketCreation, gcpBuffers) {
   std::vector<int> big(4, 40), bs(4, 2);
   big[0] = 100;
 
+  std::cerr<<"where2do i die 1 "<<std::endl;
   std::shared_ptr<SEP::IO::blocking> block(new SEP::IO::blocking(bs, big));
 
-  std::shared_ptr<ioModes> io(new ioModes());
+  std::vector<std::string> args;
+  std::shared_ptr<ioModes> io(new ioModes(args));
 
+  std::cerr<<"where3do i die 1 "<<std::endl;
   std::shared_ptr<genericIO> gcp = io->getIO("GCPBUFFERS");
   std::shared_ptr<genericRegFile> fle0;
 
-  ASSERT_NO_THROW(fleO = gcp->getGenericRegFile(bucket1, SEP::usageOut));
 
-  ASSERT_NO_THROW(fle0->writeDescription());
+  std::cerr<<"where4do i die 1 "<<std::endl;
+  ASSERT_NO_THROW(fle0 = gcp->getRegFile(bucket1, SEP::usageOut));
+
+  ASSERT_NO_THROW(fle0->setHyper(hyper));
+
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  std::cerr<<"where6do i die 1 "<<std::endl;
 
   ASSERT_NO_THROW(fle0->writeFloatStream(ar));
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+  std::cerr<<"where5do i die 1 "<<std::endl;
+  ASSERT_NO_THROW(fle0->writeDescription());
 
   ASSERT_NO_THROW(fle0->close());
 
   std::shared_ptr<genericRegFile> fle1;
 
   ASSERT_NO_THROW(
-      fle1 = gcp->getGenericRegFile("testbucket1993/testIt", SEP::usageIn));
-  ASSERT_NO_THROW(fle1->readDescription());
+      fle1 = gcp->getRegFile("testbucket1993/testIt", SEP::usageIn));
+  ASSERT_NO_THROW(fle1->readDescription(4));
 
-  ASSERT_NO_THROW(fle1->wreadFloatStream(ar));
+  ASSERT_NO_THROW(fle1->readFloatStream(ar));
   high_resolution_clock::time_point t3 = high_resolution_clock::now();
 
   ASSERT_NO_THROW(fle1->close());
 
   auto d1 = duration_cast<microseconds>(t2 - t1).count();
-  double s1 = (double)n123 * 4 / d1;
-  double s2 = (double)n123 * 4 / d2;
 
+  double s1 = (double)n123 * 4 / d1;
   std::cerr << "To buffer " << s1 << " MB/s " << std::endl;
   ;
 
   auto d2 = duration_cast<microseconds>(t3 - t2).count();
+  double s2 = (double)n123 * 4 / d2;
   std::cerr << "From cloud " << (double)n123 * 4 / d2 << " MB/s " << std::endl;
   ;
 

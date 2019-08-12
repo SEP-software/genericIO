489c770db6b70728be5f591578eff584a8637748
Robert Clapp <bob@sep.stanford.edu>
2019-08-12 21:01:22 +0000

    throw error when undefined

--- cpp/sep_reg_file.cpp
+++ cpp/sep_reg_file.cpp
@@ -514,6 +514,7 @@ void sepRegFile::readDescription(const int ndimMax) {
     error(std::string("Only know about esize=4 or 1"));
 }
 void sepRegFile::writeDescription() {
+  std::cerr << "in write description " << std::endl;
   std::shared_ptr<hypercube> hyper = getHyper();
   std::vector<axis> axes = hyper->returnAxes(hyper->getNdim());
 

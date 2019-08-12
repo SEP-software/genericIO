ecba5d91eb1b1ba70c3eae6fa410649796c841e0
Robert Clapp <bob@sep.stanford.edu>
2019-08-12 17:56:48 +0000

    throw error when undefined

--- cpp/buffersRegFile.cpp
+++ cpp/buffersRegFile.cpp
@@ -8,12 +8,15 @@ using namespace SEP;
 void buffersRegFile::writeDescription() {
   std::shared_ptr<hypercube> hyper = getHyper();
   std::vector<axis> axes = hyper->returnAxes(hyper->getNdim());
+  std::cerr<<"what is going on "<<std::endl;
   for (int i = 1; i <= axes.size(); i++) {
     putInt(std::string("n") + std::to_string(i), axes[i - 1].n);
     putFloat(std::string("o") + std::to_string(i), axes[i - 1].o);
     putFloat(std::string("d") + std::to_string(i), axes[i - 1].d);
     putString(std::string("label") + std::to_string(i), axes[i - 1].label);
   }
+  std::cerr<<"2hat is going on "<<std::endl;
   jsonArgs["bufferInfo"] = _bufs->getDescription();
+  std::cerr<<"what is going on "<<std::endl;
   jsonArgs["dataType"] = getTypeString(getDataType());
 }

06937da0b959217761ebde0b7a19f504f77b8f06
Robert Clapp <bob@roberts-mbp-3.lan>
2019-08-22 18:28:03 -0700

    submodule update

--- python/genericIO.py
+++ python/genericIO.py
@@ -50,7 +50,7 @@ class argParseParams:
     def __init__(self, inpars):
         """Create a parameter object that uses info from argsparse module"""
         self.pars = {}
-        for k, v in self.inpars.items():
+        for k, v in inpars.items():
             if v:
                 self.pars[k] = v
         self.cppMode = pyGenericIO.dictParams(self.pars)

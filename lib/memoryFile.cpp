635befd96e8649dccfb30ad5c410a66c4830da2c
Bob Clapp <bob@sep.stanford.edu>
2019-10-12 22:22:26 +0000

    submodule update

--- lib/memoryFile.cpp
+++ lib/memoryFile.cpp
@@ -474,15 +474,13 @@ void memoryRegFile::allocateCheck(dataType typ) {
   if (hyper == nullptr) throw SEPException("Hypercube not set");
   std::cerr << "not mull ptr" << std::endl;
   dataType cur = getDataType();
-  x
 
-      if (cur == DATA_UNKNOWN) {
+  if (cur == DATA_UNKNOWN) {
     setDataType(typ);
     std::cerr << "allocating to " << hyper->getN123() << " " << getDataEsize()
               << std::endl;
     _buf.resize(hyper->getN123() * getDataEsize());
-  }
-  else if (cur != typ) {
+  } else if (cur != typ) {
     throw SEPException("Can not change data type once a write has started");
     if (_buf.size() == 0) {
       _buf.resize(hyper->getN123() * getDataEsize());

034f5b89fcd932d71ea0e7c1d2d52d707c522244
Bob Clapp <bob@sep.stanford.edu>
2019-10-15 22:31:01 +0000

    submodule update

--- lib/testGCP.cpp
+++ lib/testGCP.cpp
@@ -6,13 +6,13 @@ TEST(TESTGCP, basic) {
   std::vector<std::string> args;
   args.push_back("args");
   ioModes modes(args);
-  std::shared_ptr<genericIO> io = modes->getIO("GCPBUFFERS");
+  std::shared_ptr<genericIO> io = modes.getIO("GCPBUFFERS");
 
   std::shared_ptr<genericRegFile> file =
       io->getRegFile("unitTestB/testFile", usageOut);
 
   std::shared_ptr<hypercube> hyper(new hypercube(1000, 1000, 1000));
-  std::shared_ptr<float3DReg> buf(hyper);
+  std::shared_ptr<float3DReg> buf(new float3DReg(hyper));
   for (int i = 0; i < 1000 * 1000 * 1000; i++) {
     _hyper->getVals()[i] = i;
   }

6ebe41d8a8a501d0be513d8adfc5e76a2acbfd90
Bob Clapp <bob@sep.stanford.edu>
2019-10-16 18:01:59 +0000

    submodule update

--- lib/genericFile.cpp
+++ lib/genericFile.cpp
@@ -430,7 +430,6 @@ bool genericRegFile::writeWindow(const std::vector<int> &nw,
 }
 void genericRegFile::setHyper(const std::shared_ptr<SEP::hypercube> hyp) {
   _hyper = hyp->clone();
-  std::cerr << "in set hyper" << std::endl;
   if (_hyper == nullptr)
     throw SEPException(std::string("hypercube not defined after clone"));
 }

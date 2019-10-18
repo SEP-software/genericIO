79ea0483a48f3c02ae25aa7058300f8b8118ddcb
Bob Clapp <bob@sep.stanford.edu>
2019-10-18 22:21:00 +0000

    python fixed

--- genericIOConfig.cmake
+++
@@ -1,9 +0,0 @@
-
-
-
-
-check_required_components(genericIO)
-
-if(NOT TARGET SEP:IO:genericCpp)
-  include("${CMAKE_CURRENT_LIST_DIR}/genericIOTargets.cmake")
-endif()
--- ioConfig.h
+++
@@ -1,5 +0,0 @@
-/* #undef USE_SEP */
-/* #undef USE_RSF */
-#define USE_SEPVECTOR
-/* #undef USE_GCP */
-#define DEFAULTIO "JSON"

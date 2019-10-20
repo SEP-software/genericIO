4132a41f2e37c7a05bdafe1de84977ca06b43a64
Robert Clapp <bob@roberts-mbp-3.lan>
2019-10-20 11:05:36 -0700

    get rid of numpy

--- lib/memoryFile.h
+++ lib/memoryFile.h
@@ -353,7 +353,9 @@ Read a portion of file based on window parameters
     Read the description of the file
 */
   virtual void readDescription(const int ndim=-1) override { ; };
-  virtual Json::Value getDescription() override { ; }
+  virtual Json::Value getDescription() override { 
+Json::value a;
+return a; }
   virtual void putDescription(const std::string &title,
                               const Json::Value &desc) override {
     ;

b46247666f279cd051c73adbecb06a4c25a8fad6
Robert Clapp <bob@roberts-mbp-3.lan>
2020-03-23 20:56:45 -0700

    update print

--- util/Info.py
+++ util/Info.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env @Python3_EXECUTABLE@
+import argparse
+import genericIO
+
+parser = argparse.ArgumentParser(description='Print info about files')
+parser.add_argument('strings', metavar='N', type=string, nargs='+',
+                    help='A list of files to give more info')
+args = parser.parse_args()
 
 
 

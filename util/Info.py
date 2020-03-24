4daf6708342f4ea4ab22cf98e4a33a3e45e03af4
Robert Clapp <bob@roberts-mbp-3.lan>
2020-03-23 21:08:26 -0700

    update print

--- util/Info.py
+++ util/Info.py
@@ -3,9 +3,10 @@ import argparse
 import genericIO
 
 parser = argparse.ArgumentParser(description='Print info about files')
-parser.add_argument('string', metavar='N', type=string, nargs='+',
+parser.add_argument('files', metavar='Files', type=str, nargs='+',
                     help='A list of files to give more info')
 args = parser.parse_args()
+print(args.files)
 
 
 

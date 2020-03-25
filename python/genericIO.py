061f37b42c2e191013335e929e1c28cb4b12d43b
Robert Clapp <bob@roberts-mbp-3.lan>
2020-03-24 20:47:32 -0700

    added first Sdd version

--- python/genericIO.py
+++ python/genericIO.py
@@ -107,7 +107,7 @@ class regFile:
         self.tag = tag
         self.usage = None
         ndimMax = -1
-        print(ioM)
+        print(ioM,"XXXX")
         self._type=ioM.getType()
         if "ndims" in kw:
             ndimMax = kw["ndims"]

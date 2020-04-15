252f1867c34ac8337a4f367ad8d364074ec5fd78
Robert Clapp <bob@roberts-mbp-3.lan>
2020-04-15 15:21:03 -0700

    fix addIrregFile

--- python/genericIO.py
+++ python/genericIO.py
@@ -554,6 +554,7 @@ class irregFile:
             self.cppMode = ioM.getIrregFile(
                 self.tag, usageConvert[
                     self.usage], ndimMax)
+            return;
             if vec._header._gridHyper:
                 self.cppMode.setHaveGrid(True)
             else:

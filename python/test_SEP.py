213934116c0d10c99590efef80c0e46b0f72e198
Robert Clapp <bob@roberts-mbp-3.lan>
2020-04-17 10:26:57 -0700

    debug Irreg

--- python/test_SEP.py
+++ python/test_SEP.py
@@ -22,34 +22,7 @@ def makeDataset():
     return SepIrregVector.vector(traces=traces,header=headers)
 
 class TestIrregSimple(unittest.TestCase):
-    def testSimpleIrregTraces(self):
-        io=genericIO.io("SEP")
-        dat=makeDataset()
-        try:
-            fle=io.getIrregFile("/tmp/junk.H",fromVector=dat)
-            fle.writeDataWindow(dat)
-            fle.close()
-        except:
-            self.assertTrue((1==2))
-        fle2=io.getIrregFile("/tmp/junk.H")
-        dat2=fle2.readDataWindow()
-        d1=dat.getNdArray()
-        d2=dat2.getNdArray()
-        for i2 in range(10):
-            for i1 in range(10):
-                self.assertEqual(d2[i2,i1],d1[i2,i1])
-            self.assertEqual(dat._header.s_x[i2],dat2._header.s_x[i2])
-            self.assertEqual(dat._header.s_y[i2],dat2._header.s_y[i2])
-        fle3=io.getIrregFile("/tmp/junk2.H",fromHeader=dat._header,dataIn=fle2)
-        fle3.writeHeaderWindow(dat2._header)
-        fle3.close()
-        fle4=io.getIrregFile("/tmp/junk2.H")
-        head2=fle4.readHeaderWindow()
-        for i2 in range(10):
-            self.assertEqual(dat._header.s_x[i2],head2.s_x[i2])
-            self.assertEqual(dat._header.s_y[i2],head2.s_y[i2])    
-        fle3.remove()
-        fle.remove()   
+  
 
     def testOutOfOrderTraces(self):
         io=genericIO.io("SEP")
@@ -87,27 +60,6 @@ class TestIrregSimple(unittest.TestCase):
      #   fle.remove()  
 
 
-    def testGrid(self):
-        io=genericIO.io("SEP")
-        dat=makeDataset()
-        ax=[]
-        ax.append(Hypercube.axis(n=10,o=1,d=1,label="s_y"))
-        ax.append(Hypercube.axis(n=10,o=0,d=1,label="s_x"))
-        dat2=dat.clone()
-        dat3=dat2.gridData(ax)
-        fle=io.getIrregFile("/tmp/junk5.H",fromVector=dat3)
-        fle.writeDataWindow(dat3)
-        fle.close()
-        fle2=io.getIrregFile("/tmp/junk5.H")
-        dat2=fle2.readDataWindow()
-        d1=dat.getNdArray()
-        d2=dat2.getNdArray()
-        for i2 in range(10):
-            for i1 in range(10):
-                self.assertEqual(d2[i2,i1],d1[i2,i1])
-            self.assertEqual(dat._header.s_x[i2],dat2._header.s_x[i2])
-            self.assertEqual(dat._header.s_y[i2],dat2._header.s_y[i2])
 
-        fle.remove() 
 if __name__ == '__main__':
     unittest.main()

e9e43586aee315a80f8f9f6a5ab291d0131550f9
Robert Clapp <bob@roberts-mbp-3.lan>
2020-04-17 11:30:23 -0700

    debug Irreg

--- python/genericIO.py
+++ python/genericIO.py
@@ -538,7 +538,7 @@ class irregFile:
                 hyp=Hypercube.hypercube(ns=[len(header._keyOrder),header._nh])
                 self.cppMode.setHyper(hyp.cppMode)
                 self.cppMode.setHaveGrid(False)
-            if header._drn is None:
+            if isinstance(header._drn,None):
                 self.cppMode.setInOrder(False)
                 print("IN order false",type(header._drn))
             else:

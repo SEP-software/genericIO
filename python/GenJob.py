2d5e55985f0534ea15cf4c7b643992f7e51d7cfc
Robert Clapp <bob@roberts-mbp-3.lan>
2020-04-21 21:44:18 -0700

    update pyVector

--- python/GenJob.py
+++ python/GenJob.py
@@ -318,7 +318,7 @@ class irregSpace(space):
         
             returns processed buffer
         """
-        if self._inputJob not None
+        if not self._inputJob is None
             hyp=self._hyperIn.subCube(self._nw[iwind],self._fw[iwind],self._jw[iwind])
             inBuf=self.processBuffer(None,hyp,iwind)
         elif self._hasInput:

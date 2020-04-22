b02aef5ef2b22a81fbdeb426e66ef4daf8aad800
Robert Clapp <bob@roberts-mbp-3.lan>
2020-04-22 11:53:16 -0700

    update pyVector

--- converters/segy/GenReadSegy.py
+++ converters/segy/GenReadSegy.py
@@ -13,7 +13,7 @@ import numpy as np
 from numba import jit,prange
 import ast
 
-class fromSEGY(GenJob.regSpace):
+class fromSEGY(GenJob.serialIrregDataSpace):
     def __init__(self,outputType,keys,segyfile):
         """Intialize object
 

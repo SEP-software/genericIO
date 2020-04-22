146892c505d41677946b346eb176c43fe5777c7a
Robert Clapp <bob@roberts-mbp-3.lan>
2020-04-22 14:04:54 -0700

    update pyVector

--- converters/segy/GenReadSegy.py
+++ converters/segy/GenReadSegy.py
@@ -48,7 +48,6 @@ class fromSEGY(GenJob.irregSpace):
         for i2 in range(ax.n):
             for k,v in self._keys.items():
                 i=i2+self._fw[self._iwind][1]
-                print(i,v,"SDDS",k)
                 bufsN[k][i2]=self._segyfile.header[i].get(v)
             outN[i2,:]=self._segyfile.trace[i2]
         for k in self._keys.keys():

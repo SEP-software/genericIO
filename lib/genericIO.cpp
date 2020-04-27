7d1d48ab4424b92eb2bb6a46319422b0f3db8f57
Robert Clapp <bob@roberts-mbp-3.lan>
2020-04-27 15:53:36 -0700

    update fodr sep--main

--- lib/genericIO.cpp
+++ lib/genericIO.cpp
@@ -3,6 +3,7 @@ using namespace SEP;
 void genericIO::filesClose() {
   std::cerr << "in close" << std::endl;
   for (auto i = _regFiles.begin(); i != _regFiles.end(); ++i) {
+    std::cerr << "closing " << i->first << std::endl;
     i->second->close();
   }
   for (auto i = _irregFiles.begin(); i != _irregFiles.end(); ++i) {

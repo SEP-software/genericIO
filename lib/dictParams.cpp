e9414d21f20c1a141f85212196e0ca0edffbd134
Robert Clapp <bob@roberts-mbp-3.lan>
2020-05-09 12:45:39 -0700

    debugging genericIO dict

--- lib/dictParams.cpp
+++ lib/dictParams.cpp
@@ -2,38 +2,45 @@
 #include "SEPException.h"
 using namespace SEP;
 
-dictParams::dictParams(std::map<std::string, std::string> pars) {
+dictParams::dictParams(std::map<std::string, std::string> pars)
+{
   _pars = pars;
 }
 
-void dictParams::resetParams(const std::map<std::string, std::string> pars) {
+void dictParams::resetParams(const std::map<std::string, std::string> pars)
+{
   _pars = pars;
 }
 
-void dictParams::addParams(const std::map<std::string, std::string> pars) {
+void dictParams::addParams(const std::map<std::string, std::string> pars)
+{
   for (auto it = pars.begin(); it != pars.end(); ++it)
     _pars[it->first] = it->second;
 }
 
-int dictParams::getInt(const std::string &arg) const {
+int dictParams::getInt(const std::string &arg) const
+{
   if (_pars.count(arg) == 1)
     return std::stoi(_pars.at(arg));
   error(std::string("trouble grabbing parameter ") + arg +
         std::string(" from parameters"));
   return 0;
 }
-int dictParams::getInt(const std::string &arg, const int def) const {
+int dictParams::getInt(const std::string &arg, const int def) const
+{
   if (_pars.count(arg) == 1)
     return std::stoi(_pars.at(arg));
   return def;
 }
 
-float dictParams::getFloat(const std::string &arg, const float def) const {
+float dictParams::getFloat(const std::string &arg, const float def) const
+{
   if (_pars.count(arg) == 1)
     return std::stof(_pars.at(arg));
   return def;
 }
-float dictParams::getFloat(const std::string &arg) const {
+float dictParams::getFloat(const std::string &arg) const
+{
   if (_pars.count(arg) == 1)
     return std::stof(_pars.at(arg));
 
@@ -41,11 +48,13 @@ float dictParams::getFloat(const std::string &arg) const {
         std::string(" from parameters"));
   return 0.;
 }
-void dictParams::message(const std::string &arg) const {
+void dictParams::message(const std::string &arg) const
+{
   std::cerr << arg << std::endl;
 }
 
-std::string dictParams::getString(const std::string &arg) const {
+std::string dictParams::getString(const std::string &arg) const
+{
   if (_pars.count(arg) == 1)
     return _pars.at(arg);
 
@@ -54,7 +63,8 @@ std::string dictParams::getString(const std::string &arg) const {
   return "null";
 }
 std::string dictParams::getString(const std::string &arg,
-                                  const std::string &def) const {
+                                  const std::string &def) const
+{
   if (_pars.count(arg) == 1)
     return _pars.at(arg);
   if (_pars.count(arg) == 1)
@@ -62,36 +72,48 @@ std::string dictParams::getString(const std::string &arg,
   return def;
 }
 
-bool dictParams::getBool(const std::string &arg, bool def) const {
-  if (_pars.count(arg) == 1) {
-    std::string x=_pars.at(arg);
-    if(x.length()>0){
-    std::string x2=x.substr(0,1);
-    if (x2 == "y" ||x2   == "Y" ||
-        x2 == "1" || x2 == "T" ||
-        x2 == "t")
-      return true;
-    return false;
-}
+bool dictParams::getBool(const std::string &arg, bool def) const
+{
+  if (_pars.count(arg) == 1)
+  {
+    std::string x = _pars.at(arg);
+    if (x.length() > 0)
+    {
+      std::string x2 = x.substr(0, 1);
+      if (x2 == "y" || x2 == "Y" ||
+          x2 == "1" || x2 == "T" ||
+          x2 == "t")
+        return true;
+      return false;
+    }
   }
   return def;
 }
-bool dictParams::getBool(const std::string &arg) const {
-  if (_pars.count(arg) == 1) {
-    if (_pars.at(arg).at(0) == 'y' || _pars.at(arg).at(0) == 'Y' ||
-        _pars.at(arg).at(0) == '1')
-      return true;
+bool dictParams::getBool(const std::string &arg) const
+{
+  if (_pars.count(arg) == 1)
+  {
+    std::string x = _pars.at(arg);
+    if (x.length() > 0)
+    {
+      if (x2 == "y" || x2 == "Y" ||
+          x2 == "1" || x2 == "T" ||
+          x2 == "t")
+        return true;
+    }
     return false;
   }
   error(std::string("troublue grabbing parameter ") + arg +
         std::string(" from parameters"));
   return false;
 }
-std::vector<std::string> dictParams::splitString(const std::string &str) const {
+std::vector<std::string> dictParams::splitString(const std::string &str) const
+{
   std::string delim = ",";
   std::vector<std::string> tokens;
   size_t prev = 0, pos = 0;
-  do {
+  do
+  {
     pos = str.find(delim, prev);
     if (pos == std::string::npos)
       pos = str.length();
@@ -104,66 +126,81 @@ std::vector<std::string> dictParams::splitString(const std::string &str) const {
 }
 
 std::vector<int> dictParams::getInts(const std::string &arg,
-                                     const int nvals) const {
-  if (_pars.count(arg) == 0) {
+                                     const int nvals) const
+{
+  if (_pars.count(arg) == 0)
+  {
     error(std::string("troubl3 grabbing parameter ") + arg +
           std::string(" from parameters"));
   }
 
   std::vector<int> array;
   std::vector<std::string> strs = splitString(_pars.at(arg));
-  for (auto i = 0; std::min((int)strs.size(), nvals); i++) {
+  for (auto i = 0; std::min((int)strs.size(), nvals); i++)
+  {
     array.push_back(std::stoi(strs[i]));
   }
   return array;
 }
 std::vector<int> dictParams::getInts(const std::string &arg,
-                                     const std::vector<int> &defs) const {
-  if (_pars.count(arg) == 0) {
+                                     const std::vector<int> &defs) const
+{
+  if (_pars.count(arg) == 0)
+  {
     return defs;
   }
 
   std::vector<int> array;
   std::vector<std::string> strs = splitString(_pars.at(arg));
-  for (auto i = 0; strs.size(); i++) {
+  for (auto i = 0; strs.size(); i++)
+  {
     array.push_back(std::stoi(strs[i]));
   }
   return array;
 }
 
 std::vector<float> dictParams::getFloats(const std::string &arg,
-                                         const int nvals) const {
-  if (_pars.count(arg) == 0) {
+                                         const int nvals) const
+{
+  if (_pars.count(arg) == 0)
+  {
     error(std::string("tromuble grabbing parameter ") + arg +
           std::string(" from parameters"));
   }
 
   std::vector<float> array;
   std::vector<std::string> strs = splitString(_pars.at(arg));
-  for (auto i = 0; std::min((int)strs.size(), nvals); i++) {
+  for (auto i = 0; std::min((int)strs.size(), nvals); i++)
+  {
     array.push_back(std::stof(strs[i]));
   }
   return array;
 }
 std::vector<float> dictParams::getFloats(const std::string &arg,
-                                         const std::vector<float> &defs) const {
-  if (_pars.count(arg) == 0) {
+                                         const std::vector<float> &defs) const
+{
+  if (_pars.count(arg) == 0)
+  {
     return defs;
   }
 
   std::vector<float> array;
   std::vector<std::string> strs = splitString(_pars.at(arg));
-  for (auto i = 0; strs.size(); i++) {
+  for (auto i = 0; strs.size(); i++)
+  {
     array.push_back(std::stof(strs[i]));
   }
   return array;
 }
-void dictParams::addParams(std::map<std::string, std::string> &pars) {
-  for (auto p = pars.begin(); p != pars.end(); p++) {
+void dictParams::addParams(std::map<std::string, std::string> &pars)
+{
+  for (auto p = pars.begin(); p != pars.end(); p++)
+  {
     _pars[p->first] = p->second;
   }
 }
 
-void dictParams::error(const std::string &errm) const {
+void dictParams::error(const std::string &errm) const
+{
   throw SEPException(errm);
 }

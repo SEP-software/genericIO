746fcfafc78af14075ecca99743d72741ce2187b
Robert Clapp <bob@roberts-mbp-3.lan>
2020-07-30 10:13:37 -0700

    debug

--- converters/segy/GenReadSegy.py
+++ converters/segy/GenReadSegy.py
@@ -49,11 +49,11 @@ class fromSEGY(GenJob.irregSpace):
             for k,v in self._keys.items():
                 i=i2+self._fw[self._iwind][1]
                 bufsN[k][i2]=self._segyfile.header[i].get(v)
-                print(i,i2,k,bufsN[k][i2],v)
             outN[i2,:]=self._segyfile.trace[i]
-        raise Exception("Stop")
         for k in self._keys.keys():
             outa._header.setKey(k,bufsS[k].getNdArray())
+        print(outa._header.getKey("CDP")[0:50])
+        raise Exception("eror")
         return outa
 
 

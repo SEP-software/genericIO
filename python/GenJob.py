f23d6b09a1ff45715e66e726df23fce0a96a9109
Robert Clapp <bob@roberts-mbp-3.lan>
2020-07-31 16:20:45 -0700

    add GenDis3dHead

--- python/GenJob.py
+++ python/GenJob.py
@@ -319,14 +319,16 @@ class irregSpace(space):
         
             returns processed buffer
         """
-
+        print("WHere 1")
         if not self._inputJob is None:
             print("WHAT THE",self._nw[iwind],self._fw[iwind],self._jw[iwind])
             hyp=self._hyperIn.subCube(self._nw[iwind],self._fw[iwind],self._jw[iwind])
             inBuf=self.processBuffer(None,hyp,iwind)
         elif self._hasInput:
+            print("WHere 2")
             inBuf=bufIn
         else:
+            print("WHere 3")
             inBuf=None
         outBuf=self.makeOutputBuffer(inBuf,hyperOut)
         self.process(inBuf,outBuf)

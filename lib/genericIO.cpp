be856f07116f2b33e3ce93a37c4b85f698b070ff
Robert Clapp <bob@roberts-mbp-3.lan>
2020-08-03 09:25:43 -0700

    add GenDis3dHead

--- lib/genericIO.cpp
+++ lib/genericIO.cpp
@@ -1,16 +1,24 @@
 #include "genericIO.h"
 using namespace SEP;
-void genericIO::filesClose() {
-  for (auto i = _regFiles.begin(); i != _regFiles.end(); ++i) {
+void genericIO::filesClose()
+{
+  std::cerr << "in files close" << std::endl;
+  for (auto i = _regFiles.begin(); i != _regFiles.end(); ++i)
+  {
+    fprintf(stderr, "in loop 1 \n");
     i->second->close();
   }
-  for (auto i = _irregFiles.begin(); i != _irregFiles.end(); ++i) {
+  for (auto i = _irregFiles.begin(); i != _irregFiles.end(); ++i)
+  {
+    fprintf(stderr, "in loop 2 \n");
     i->second->close();
   }
+  fprintf(stderr, "through \n");
 }
 std::shared_ptr<SEP::genericRegFile>
 genericIO::getRegFile(const std::string &name, const SEP::usage_code usage,
-                      const int ndimMax) {
+                      const int ndimMax)
+{
   std::shared_ptr<paramObj> par = getParamObj();
   std::string filename = par->getString(name, name);
   return getRegFileTag(name, filename, usage, ndimMax);
@@ -18,20 +26,25 @@ genericIO::getRegFile(const std::string &name, const SEP::usage_code usage,
 
 std::shared_ptr<SEP::genericRegFile>
 genericIO::getDocRegFile(const std::string &name, const std::string &doc,
-                         const SEP::usage_code usage, const int ndimMax) {
+                         const SEP::usage_code usage, const int ndimMax)
+{
   std::shared_ptr<paramObj> par = getParamObj();
   std::string filename = par->getString(name, name);
   std::shared_ptr<SEP::genericRegFile> v;
-  try {
+  try
+  {
     v = getRegFileTag(name, filename, usage, ndimMax);
-  } catch (SEPException &x) {
+  }
+  catch (SEPException &x)
+  {
     std::string tmp = std::string(x.what()) + std::string("\n\t") + doc;
     throw SEPException(tmp);
   }
   return v;
 }
 
-void genericIO::removeRegFile(const std::string fle) {
+void genericIO::removeRegFile(const std::string fle)
+{
   if (_regFiles.count(fle) == 0)
     throw SEPException(std::string("Requested to remove file ") + fle +
                        std::string("that has not been initialized"));
@@ -41,11 +54,15 @@ void genericIO::removeRegFile(const std::string fle) {
 
 std::shared_ptr<SEP::genericRegFile>
 genericIO::getDocRegFile(const std::string &name, const std::string &doc,
-                         const std::string usage, const int ndimMax) {
+                         const std::string usage, const int ndimMax)
+{
   std::shared_ptr<SEP::genericRegFile> v;
-  try {
+  try
+  {
     v = getRegFile(name, usage, ndimMax);
-  } catch (SEPException &x) {
+  }
+  catch (SEPException &x)
+  {
     std::string tmp = std::string(x.what()) + std::string("\n\t") + doc;
     throw SEPException(tmp);
   }
@@ -54,14 +71,16 @@ genericIO::getDocRegFile(const std::string &name, const std::string &doc,
 
 std::shared_ptr<SEP::genericIrregFile>
 genericIO::getIrregFile(const std::string &name, const SEP::usage_code usage,
-                        const int ndimMax) {
+                        const int ndimMax)
+{
   std::shared_ptr<paramObj> par = getParamObj();
   std::string filename = par->getString(name, name);
 
   return getIrregFileTag(name, filename, usage, ndimMax);
 }
 void genericIO::fileDebug(const std::string nm, const float *data, const int n1,
-                          const int n2, const int n3) {
+                          const int n2, const int n3)
+{
   std::vector<SEP::axis> axs;
   axs.push_back(SEP::axis(n1));
   axs.push_back(SEP::axis(n2));
@@ -74,7 +93,8 @@ void genericIO::fileDebug(const std::string nm, const float *data, const int n1,
   fle->writeFloatStream(data, (long long)n1 * (long long)n2 * (long long)n3);
 }
 void genericIO::fileDebug(const std::string nm, const float *data, const int n1,
-                          const int n2) {
+                          const int n2)
+{
   std::vector<SEP::axis> axs;
   axs.push_back(SEP::axis(n1));
   axs.push_back(SEP::axis(n2));
@@ -88,7 +108,8 @@ void genericIO::fileDebug(const std::string nm, const float *data, const int n1,
 
 void genericIO::addFileDescription(const std::shared_ptr<genericRegFile> fileIn,
                                    const std::string &title,
-                                   std::shared_ptr<genericRegFile> fileOut) {
+                                   std::shared_ptr<genericRegFile> fileOut)
+{
   Json::Value val = fileIn->getDescription();
   fileOut->putDescription(title, val);
 }

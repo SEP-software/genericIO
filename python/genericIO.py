d6a883dca1ab9c9117236e5afd8c96f0cfe12a98
Robert Clapp <bob@roberts-mbp-3.lan>
2020-08-02 19:11:04 -0700

    add GenDis3dHead

--- python/genericIO.py
+++ python/genericIO.py
@@ -879,6 +879,7 @@ class irregFile:
             n1-=2
         elif typ[klast]=="dataByte":
             n1-=3
+        print("CREATING HEADER ",n1,headS._nh)
         head=SepVector.getSepVector(storage="dataByte",ns=[n1,headS._nh])
         for k,v in off.items():     
             x=SepVector.getSepVector(headS._keys[k]._vals)

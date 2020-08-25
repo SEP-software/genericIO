9766ed9df923e1de2b339508653fcc88c1ff9eb4
Robert Clapp <bob@roberts-mbp-3.lan>
2020-08-25 12:56:50 -0700

    Add window key

--- util/header/GenWindowKey.py
+++ util/header/GenWindowKey.py
@@ -12,48 +12,82 @@ import threading
 mins={}
 maxs={}
 class windowKey(GenJob.irregSpace):
-    def __init__(self,inputType,first,last,keys):
+    def __init__(self,inputType,synch, mins,maxs):
         """Intialize object
 
             inputType - Input type
-            first - First key to print
-            last - Last key to print
-            keys  - List of keys to print
+            synch - Whether or not we are synching headers and data
+            mins - Dictionary containing the minimum for each key
+            maxs - Dictionary containing the maximim for each key
         """
-        super().__init__(self.printInfo,0,0,inputType=inputType,hasOutput=False)
+        super().__init__(self.windowIt,0,0,inputType=inputType,hasOutput=False)
         self._lock=threading.Lock() 
-        self._first=first
-        self._print=True
-        self._last=last
-        self._keys=copy.deepcopy(keys)
+        self._mins=copy.deepcopy(mins)
+        self._maxs=copy.deepcopy(maxs)
+        self._synch=synch
         self._done=0
-    def printInfo(self,ina,dummy):
-        """Convert a buffer from one type to another
+        self._typ=inputType
+        
+    def windowIt(self,ina,outa):
+        """Window a buffer
 
         ina - Input vector
-        dummy - Dummy argument
+        outa - Output vector
         """
-        f2=self._done
+        keep = np.ones((ina._header._nh,), dtype=bool)
+        for k,v in self._mins.items():
+            keep,nc=parseIt(ina._header.getKey(k)._vals,keep,v,self._maxs[k])
+        if nc>0:
+            outa.noGrid()
+            outa.shrinkZero(nc)
+            head=SepIrregVector.headerBlock(nc,drn=ina._drn,keyOrder=ina._keyOrder)
+            parseKey(keep,ina._header.getKey(k)._vals,head.getkey(k)._vals)
+            if self._synch:
+                parseTraces(keep,ina._traces,outa._traces)
+        
 
-        for i in range(ina._header._nh):
-            if f2 +i >= self._first and f2+i < self._last:
-                if i%20==0 of self._print:
-                    self._print=False
-                    line="Number       "
-                    for k in self._keys:
-                            x=str(k)
-                            if len(x)>10:
-                                x=x[0:10]+" "
-                            line+=x+" "*(11-len(x))
-                    print(line)
-                line=str(f2+i)
-                line+=" "*(13-len(line))   
-                for k in self._keys:
-                    x=str(ina._header.getKey(k)[i])
-                    line+=x+" "*(11-len(x))
-                print(line)
-        self._done+=ina._header._nh
-                    
+                
+
+
+ 
+
+@numba.jit(nopython=True)
+def parseIt(vals,keep,mn,mx):
+    """
+      Return whether or not a trace is within window
+      """
+    nc=0
+    for i in range(vals.shape[0]):
+        if vals[i] < mn:
+            keep[i]=False
+        else if vals[i] > mx:
+            keep[i]=False
+        if keep[i]:
+            nc=nc+1
+    return keep,nc
+
+@numba.jit(nopython=True)
+def parseKey(keep,inA,outA):
+    """
+     Put subset of inA (controled by keep) into outA
+      """
+    nc=0
+    for i in range(inA.shape[0]):
+        if keep[i]:
+            outA[nc]=inA[i]
+            nc+=1
+
+@numba.jit(nopython=True)
+def parseTraces(keep,inA,outA):
+    """
+     Put subset of inA (controled by keep) into outA
+      """
+    nc=0
+    for i in range(inA.shape[0]):
+        if keep[i]:
+            outA[nc,:]=inA[i,:]
+            nc+=1
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Print header attributes')
     parser.add_argument('input', metavar='Input', type=str,
@@ -87,7 +121,7 @@ if __name__ == "__main__":
     
  
 
-    job=windowJob(inFile.getStorageType(),mins,maxs)
+    job=windowJob(inFile.getStorageType(),args.synch,mins,maxs)
     job.setCompleteHyperOut(inFile.getHyper())
     job.setInputFile(inFile)
     if args.synch:
